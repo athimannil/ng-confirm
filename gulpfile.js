082493b8fe6bcb83037b88aec371b349c373adef
Muhammed Athimannil <hello@athimannil.com>
2016-02-04 18:22:23 +0400

    Compline gulp and npm files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     bower = require('gulp-bower'),
+	flatten = require('gulp-flatten'),
 	// uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     // concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
     notify = require("gulp-notify"),
 	prefixer = require('gulp-autoprefixer'),
 	livereload = require('gulp-livereload');
@@ -16,10 +18,14 @@ var config = {
 	bowerDir: './bower_components'
 };
 // Install bower
-/*gulp.task('bower', function() {
-    return bower()
-    	.pipe(gulp.dest(config.bowerDir));
-});*/
+gulp.task('bower', function() {
+	return bower();
+});
+gulp.task('bowerFiles', function() {
+    return gulp.src('bower_components/**/*.min.js')
+        .pipe(flatten())
+        .pipe(gulp.dest('./public/js'));
+});
 gulp.task('files', function() {
     return gulp.src([
 	    	'resources/**.html'
@@ -27,6 +33,14 @@ gulp.task('files', function() {
         .pipe(gulp.dest('./public'))
 		.pipe(livereload());
 });
+gulp.task('connect', function() {
+	connect.server({
+		root: 'public',
+		port: 8000,
+		livereload: true
+	});
+});
+
 // Move font to public folder
 /*gulp.task('fonts', function() {
     return gulp.src([
@@ -74,6 +88,6 @@ gulp.task('watch', function() {
 });
 
 // gulp.task('install', ['bower', 'fonts', 'images']);
-gulp.task('install', ['files']);
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('install', ['files', 'bowerFiles']);
+gulp.task('default', ['scripts', 'styles', 'watch', 'connect']);
 
