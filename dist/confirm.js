96feace82617333f5122d0c0d21c4114799036f0
Muhammed Athimannil <hello@athimannil.com>
2016-02-05 19:43:01 +0400

    simplify the code

--- dist/confirm.js
+++ dist/confirm.js
@@ -3,15 +3,24 @@ angular.module('ngConfirm', [])
     return {
       // So the link function is run before ngClick's, which has priority 0
       priority: -1,
-
       link: function(scope, element, attrs) {
         element.on('click', function(e) {
           // Don't run ngClick's handler
           e.stopImmediatePropagation();
-
           $uibModal.open({
             template: "<div class='modal-header'><h4 class='modal-title'>{{ngTitle}}</h4></div><div class='modal-body'><p>{{ngMessage}}</p></div><div class='modal-footer'><button class='btn btn-link' ng-click='no()'>Cancel</button><button class='btn' ng-class='ngButton' ng-click='yes()'>{{ngButtonText}}</button></div>",
-            controller: 'ngConfirmController',
+            controller:   function($scope, $uibModalInstance, message) {
+                $scope.ngTitle = message.ngConfirmTitle ? message.ngConfirmTitle : 'Delete';
+                $scope.ngMessage = message.ngConfirm ? message.ngConfirm : 'Are you sure you want to delete ?';
+                $scope.ngButton = message.ngConfirmType ? 'btn-' + message.ngConfirmType : 'btn-danger';
+                $scope.ngButtonText = message.ngConfirmButton ? message.ngConfirmButton : 'Delete';
+                $scope.yes = function() {
+                  $uibModalInstance.close();
+                };
+                $scope.no = function() {
+                  $uibModalInstance.dismiss();
+                };
+              },
             size: 'sm',
             resolve: {
               message: function() {
@@ -26,17 +35,5 @@ angular.module('ngConfirm', [])
           });
         });
       }
-    };
-  })
-  .controller('ngConfirmController', function($scope, $uibModalInstance, message) {
-    $scope.ngTitle = message.ngConfirmTitle ? message.ngConfirmTitle : 'Delete';
-    $scope.ngMessage = message.ngConfirm ? message.ngConfirm : 'Are you sure you want to delete ?';
-    $scope.ngButton = message.ngConfirmType ? 'btn-' + message.ngConfirmType : 'btn-danger';
-    $scope.ngButtonText = message.ngConfirmButton ? message.ngConfirmButton : 'Delete';
-    $scope.yes = function() {
-      $uibModalInstance.close();
-    };
-    $scope.no = function() {
-      $uibModalInstance.dismiss();
     };
   });
